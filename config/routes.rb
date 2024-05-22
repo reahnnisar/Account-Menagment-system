bea1beaa6d4e9b5c67173247eb5aa3152aea79c5
Rehan <rehannisar06@gmail.com>
2024-05-22 12:59:49 +0500

    some changes for new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'entries#sum'
-  
-  resources :users
+
+  resources :users, except: [:new]
   resources :entries, except: [:destroy]
-  get '/entries/:id/destroy' => 'entries#destroy', as: :destroy_entry 
-  
+  get '/entries/:id/destroy' => 'entries#destroy', as: :destroy_entry
+
   get "login" => "user_sessions#new", as: :login
   post "login" => "user_sessions#create"
   post "logout" => "user_sessions#destroy", as: :logout
